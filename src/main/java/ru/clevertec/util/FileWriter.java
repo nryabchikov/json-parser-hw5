f5fae0be4c7f3130a273a03cee0c942983216741
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-09-20 13:31:37 +0300

    added fileWriter(write string-json to file)

---
+++ src/main/java/ru/clevertec/util/FileWriter.java
@@ -0,0 +1,14 @@
+package ru.clevertec.util;
+
+import java.io.IOException;
+import java.io.Writer;
+
+public class FileWriter {
+    public static void writeToFile(String filename, String str) {
+        try (Writer writer = new java.io.FileWriter(filename)) {
+            writer.write(str);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
