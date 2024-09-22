80ae5a9d8b3da8640bd4e6908a600cac283fbcbf
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-09-22 12:03:42 +0300

    added main(test my toJson and fromJson methods and compare with jackson transformation)

--- src/main/java/ru/clevertec/Main.java
+++ src/main/java/ru/clevertec/Main.java
@@ -1,15 +1,15 @@
 package ru.clevertec;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.clevertec.config.JacksonConfig;
 import ru.clevertec.domain.Customer;
 import ru.clevertec.util.DataGenerator;
-import ru.clevertec.util.FileWriter;
 import ru.clevertec.util.JsonParser;
+import ru.clevertec.util.io.FileReader;
+import ru.clevertec.util.io.FileWriter;
 
 public class Main {
-    public static void main(String[] args) throws IllegalAccessException, JsonProcessingException {
+    public static void main(String[] args) throws Exception {
         final String FILEPATH_TO_CUSTOM_OUTPUT = "src/main/resources/custom.json";
         final String FILEPATH_TO_JACKSON_OUTPUT = "src/main/resources/jackson.json";
 
@@ -18,8 +18,20 @@ public class Main {
         JsonParser parser = new JsonParser();
 
         StringBuilder sb = parser.toJson(customer);
-        sb.deleteCharAt(sb.length() - 1).append("}");
+        sb.append("}");
         FileWriter.writeToFile(FILEPATH_TO_CUSTOM_OUTPUT, sb.toString());
         FileWriter.writeToFile(FILEPATH_TO_JACKSON_OUTPUT, objectMapper.writeValueAsString(customer));
+
+        String json = FileReader.readFromFile(FILEPATH_TO_CUSTOM_OUTPUT);
+
+        Customer resultCustomer = parser.fromJson(json, Customer.class);
+        Customer jacksonCustomer = objectMapper.readValue(json, Customer.class);
+
+        System.out.println("myCustomer after toJson and from json transformation: " + resultCustomer);
+        System.out.println("jacksonCustomer after toJson and from json transformation: " + jacksonCustomer);
+        System.out.println("Random input customer equals to  myCustomer after toJson and from json transformation: "
+                + customer.equals(resultCustomer));
+        System.out.println("jacksonCustomer after toJson and from json transformation equals to " +
+                "myCustomer after toJson and from json transformation: " + jacksonCustomer.equals(resultCustomer));
     }
 }
